b8cba6b32bdfa4ec742eb319064f2be2aad7dd41
explodegr <ergiant1109@icloud.com>
2026-03-29 00:04:02 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bot is running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Web server running on port ${PORT}`);
+});
 const { Client, GatewayIntentBits, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
 
 const client = new Client({
